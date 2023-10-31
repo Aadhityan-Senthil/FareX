af99f1d575bc43ae28e7294feaf72f8de24e634b
Aadhityan <148467879+Aadhityan-Senthil@users.noreply.github.com>
2023-10-31 19:52:55 +0530

    Add files via upload

---
+++ Graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+df = pd.read_excel("a1_FlightFare_Dataset.xlsx")
+print(df)
+plt.subplots(figsize=(14,7))
+sns.barplot(x=Airline, y=Price,ec = "black",palette="Set2")
+plt.title("Airlines Company vs Flight Ticket Price", weight="bold",fontsize=20, pad=20)
+plt.ylabel("Flight Ticket Price", weight="bold", fontsize=15)
+plt.xlabel("Airlines Name", weight="bold", fontsize=16)
+plt.xticks(rotation=90)
+plt.show()
